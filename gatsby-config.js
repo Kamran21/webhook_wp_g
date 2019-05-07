a04cb80e3350ae8dfbacdfd1e34d2e2de3146aea
Kamran Irani <kamran@Kamrans-MacBook-Pro.local>
2019-05-07 20:08:08 +0100

    Added sass and bootstrap support

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,8 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    // in gatsby-config.js
+    `gatsby-plugin-sass`,
     {
       resolve: "gatsby-source-wordpress",
       options: {
--- src/templates/page.js
+++ src/templates/page.js
@@ -3,6 +3,6 @@ import React from "react"
 export default ({ pageContext }) => {
   console.log(JSON.stringify(null, pageContext, 3))
   return (
-    <div dangerouslySetInnerHTML={{__html:pageContext.content}}/>
+    <div className="container" dangerouslySetInnerHTML={{__html:pageContext.content}}/>
   )
 }
